441cfb7cf79b98ff12f84609c2ae34b5c9d49bc2
Zakii <zakii@360-search.com>
2026-05-19 08:10:12 -0700

    Add files via upload

--- compiler/compiler.js
+++ compiler/compiler.js
@@ -1,36 +1,20 @@
-import { lex } from './lexer.js'
-import { parse } from './parser.js'
 import { compileHTML } from './htmlCompiler.js'
 import { compileJS } from './jsCompiler.js'
 import { compileThread } from './threadCompiler.js'
 
 export function compile(source) {
 
-  const tokens = lex(source)
-
-  const ast = parse(tokens)
-
-  const html = compileHTML(ast)
-
-  const js = compileJS(ast)
-
-  const css = compileThread(source)
-
-  const finalHTML = `
-${html}
-
-<style>
-${css}
-</style>
-
-<script>
-${js}
-</script>
-`
-
-  return {
-    html: finalHTML,
-    js,
-    css
+  if (source.startsWith('@import c-nuget body')) {
+    return compileHTML(source)
   }
+
+  if (source.startsWith('@import Weave ff')) {
+    return compileJS(source)
+  }
+
+  if (source.startsWith('@import Thread style')) {
+    return compileThread(source)
+  }
+
+  throw new Error('Unknown .web import type')
 }
